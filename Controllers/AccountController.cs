8299748a2692322fe78f2b6f874fe782843d82de
Aakash7khadka <aakash7khadka@gmail.com>
2021-06-14 16:33:48 +0545

    client side validation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -35,13 +35,17 @@ namespace smartpalika.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginVM obj)
+        public async Task<IActionResult> Login(LoginVM obj,string returnUrl)
         {
             if (ModelState.IsValid)
             {
                 var result = await signInManager.PasswordSignInAsync(obj.Email, obj.Password, obj.Rememberme,false);
                 if (result.Succeeded)
                 {
+                    if(! string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return LocalRedirect(returnUrl);
+                    }
                     return RedirectToAction("index", "Home");
                 }
                 ModelState.AddModelError("", "Invalid login attempt");
