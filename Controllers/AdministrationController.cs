1811924a770c192a546aff9f340721e01534d41b
Aakash7khadka <aakash7khadka@gmail.com>
2021-06-16 11:58:34 +0545

    edit roles

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -13,10 +13,12 @@ namespace smartpalika.Controllers
     public class AdministrationController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<IdentityUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         public IActionResult CreateRole()
         {
@@ -47,6 +49,59 @@ namespace smartpalika.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        public async Task<IActionResult> EditRole(Guid id)
+        {
+            var role = await roleManager.FindByIdAsync(id.ToString());
+            if (role == null)
+            {
+                string error= $"Cannot find the role with id:{id}";
+                return RedirectToAction(nameof(NotFound),new { error=error });
+            }
+            EditRoleVM model = new EditRoleVM(){
+                Id = id,
+                RoleName=role.Name
+            };
+
+            foreach(var user in userManager.Users)
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name)){
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+           
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleVM obj)
+        {
+            var role = await roleManager.FindByIdAsync(obj.Id.ToString());
+            if (role == null)
+            {
+                string msg= $"Cannot find the role with id:{obj.Id}";
+                return RedirectToAction(nameof(NotFound), new { error = msg });
+            }
+            else
+            {
+                role.Name = obj.RoleName;
+                var result=await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRole");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(obj);
+           
+        }
+        public IActionResult NotFound(string error)
+        {
+            TempData["ErrorMessage"] = error;
+            return View();
         }
     }
 }
---
+++ Models/EditRoleVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smartpalika.Models
+{
+    public class EditRoleVM
+    {
+        public EditRoleVM()
+        {
+            Users = new List<string>();
+        }
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage ="Role Name is required")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
