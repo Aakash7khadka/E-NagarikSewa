73a53a598a1d324a6a5af273455f434b63b14316
Aakash7khadka <aakash7khadka@gmail.com>
2021-07-14 16:48:10 +0545

    bugfixes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using smartpalika.Models;
@@ -13,20 +14,32 @@ namespace smartpalika.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
             _db = db;
+            _userManager = userManager;
+          
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             TimeZoneInfo Nepal_Standard_Time = TimeZoneInfo.FindSystemTimeZoneById("Nepal Standard Time");
             DateTime dateTime_Nepal = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, Nepal_Standard_Time);
             //DateTime date = DateTime.Today;
             var dateTime_ = dateTime_Nepal.ToString("yyyy/MM/dd");
-            IEnumerable<AppointmentUserDetails> data = _db.Appointment.Where(s => s.Date.Contains(dateTime_));
+            var current_user = await _userManager.GetUserAsync(User);
+            IEnumerable<AppointmentUserDetails> data=null;
+            if (User.IsInRole("Employee"))
+            {
+                data = _db.Appointment.Where(s => s.Date.Contains(dateTime_));
+            }
+            else if(User.Identity.IsAuthenticated)
+            {
+               data = _db.Appointment.Where(s => s.Date.Contains(dateTime_) && s.Email==current_user.Email);
+            }
             return View(data);
         }
 
